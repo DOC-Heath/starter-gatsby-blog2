dc525c162122bebb93f7b8e879fb3d1f240b792f
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-07-30 14:06:27 -0500

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -7,7 +7,7 @@ const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
       <span className={styles.logo} />
-      <span className={styles.logoNameAddress}">
+      <span className={styles.logoNameAddress}>
         <span className={styles.navigationItem+' '+styles.logoName}>First Christian Church</span>
         <span className={styles.navigationItem+' '+styles.logoAddress}>Granbury, Texas</span>
       </span>
