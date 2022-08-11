e02c164ea0161707e3e40994370d34d485b54ee7
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-11 14:15:04 -0500

    Update hero.js (#9)
    * Update hero.js
    
    * Update index.js
    
    * Update hero.module.css
    
    * Update hero.module.css
    
    * Update hero.module.css
    
    * Update hero.module.css
    
    * Update hero.module.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ class RootIndex extends React.Component {
       <Layout location={this.props.location}>
         <Hero
           image={author.heroImage.gatsbyImageData}
-          title={author.title}
+          title={''}
           content={author.description}
         />
         <ArticlePreview posts={posts} />
