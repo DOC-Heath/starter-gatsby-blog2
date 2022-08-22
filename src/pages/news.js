ed0dd159fc804fee87c28c4fd65b7be9d35d2271
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-22 08:19:28 -0500

    Create news.js

---
+++ src/pages/news.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+
+import Seo from '../components/seo'
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import ArticlePreview from '../components/article-preview'
+
+class NewsIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.allContentfulNews.nodes')
+
+    return (
+      <Layout location={this.props.location}>
+        <Seo title="News" />
+        <Hero title="News" />
+        <ArticlePreview posts={posts} />
+      </Layout>
+    )
+  }
+}
+
+export default NewsIndex
+
+export const pageQuery = graphql`
+  query NewsIndexQuery {
+    allContentfulNews(sort: { fields: [publishDate], order: DESC }) {
+      nodes {
+        title
+        slug
+        publishDate(formatString: "MMMM Do, YYYY")
+        tags
+        heroImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 424
+            height: 212
+          )
+        }
+        description {
+          raw
+        }
+      }
+    }
+  }
+`
