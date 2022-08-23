bd254c6e1fc9ae6d7f01ae320ae7c90b6ae7d214
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-23 11:39:42 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export const pageQuery = graphql`
   query HomeQuery {
     allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
       nodes {
-        type: blog
+        type: 'blog'
         title
         slug
         publishDate(formatString: "MMMM Do, YYYY")
@@ -50,7 +50,7 @@ export const pageQuery = graphql`
     }
     allContentfulNews( sort: { fields: [publishDate], order: ASC }) {
       nodes {
-        type: news
+        type: 'news'
         title
         slug
         publishDate(formatString: "MMMM Do, YYYY")
