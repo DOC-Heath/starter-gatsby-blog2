aa9ad80a627b6bb20793d8c358a4922c909f2357
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-24 13:26:52 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,10 @@ import Layout from '../components/layout'
 import Hero from '../components/hero'
 import ArticlePreview from '../components/article-preview'
 
-// trigger build
-
 class RootIndex extends React.Component {
   render() {
-    const posts = get(this, 'props.data.allContentfulNews.nodes')
-    const [author] = get(this, 'props.data.allContentfulNews.nodes')
+    const posts = get(this, 'props.data.allContentfulAsset.nodes')
+    // const [author] = get(this, 'props.data.allContentfulNews.nodes')
 
     return (
       <Layout location={this.props.location}>
@@ -51,6 +49,26 @@ export const pageQuery = graphql`
       }
     }
     allContentfulNews( sort: { fields: [publishDate], order: ASC }) {
+      nodes {
+        path
+        title
+        slug
+        publishDate(formatString: "MMMM Do, YYYY")
+        tags
+        heroImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 424
+            height: 212
+          )
+        }
+        description {
+          raw
+        }
+      }
+    }
+    allContentfulAsset( sort: { fields: [publishDate], order: ASC }) {
       nodes {
         path
         title
