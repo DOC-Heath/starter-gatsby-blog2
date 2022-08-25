3852b54ed46270ed6dd2cf273387c454d260da11
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-25 10:07:21 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ import Layout from '../components/layout'
 import Hero from '../components/hero'
 import ArticlePreview from '../components/article-preview'
 
-// trigger build
-
 class RootIndex extends React.Component {
   render() {
     const posts = get(this, 'props.data.posts.nodes')
