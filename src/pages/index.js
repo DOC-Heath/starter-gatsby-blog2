2a33c1c47c8a6585b3aed6f24f37a0b6aa88b401
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-08-26 11:11:27 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,6 +30,7 @@ export const pageQuery = graphql`
   query HomeQuery {
     posts: allContentfulPost( 
       sort: { fields: [publishDate], order: ASC }
+      filter: { metadata: { tags: { elemMatch: { contentful_id: { eq: "indexHome" } } } } }
     ) {
       nodes {
         path
