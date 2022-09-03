876b9910160f37a32adc019acae5f890d7442426
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-03 13:03:39 -0500

    Create live.js

---
+++ src/pages/live.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+
+import Layout from '../components/layout'
+import Hero from '../components/hero'
+import ArticlePreview from '../components/article-preview'
+import * as styles from '../templates/blog-post.module.css'
+
+class RootIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.posts.nodes')
+    const [billboard] = get(this, 'props.data.welcome.nodes')
+    
+    return (
+      <Layout location={this.props.location}>
+        <Hero
+          image={billboard.heroImage.gatsbyImageData}
+          title={billboard.title}
+          content={billboard.description}
+        />
+        <div className={styles.article}>
+          <div className={styles.body}>
+            {billboard.body?.raw && renderRichText(billboard.body)}
+          </div>
+        </div>
+        <ArticlePreview posts={posts} />
+      </Layout>
+    )
+  }
+}
+
+export default RootIndex
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    posts: allContentfulPost(
+      sort: { fields: [startDate], order: ASC }
+      filter: { index: { eq: "live" } }
+    ) {
+      nodes {
+        path
+        title
+        slug
+        videoUrl
+        startTime
+        publishDate(formatString: "MMMM Do, YYYY")
+        metadata { tags { name, id } }
+        heroImage {
+          gatsbyImageData(
+            layout: FULL_WIDTH
+            placeholder: BLURRED
+            width: 424
+            height: 212
+          )
+        }
+        description {
+          raw
+        }
+      }
+    }
+    welcome: allContentfulPost( filter: { slug: { eq: "welcome" } } ) {
+      nodes {
+        title
+        description { 
+          raw 
+        }
+        body { 
+          raw 
+        }
+        heroImage {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            width: 1180
+          )
+        }
+      }
+    }
+  }
+`
