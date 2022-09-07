84bf777f05f4ea8d1a3b1f2c4c7ec8c33a4f85e9
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-07 12:56:31 -0500

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,20 +5,20 @@ import { renderRichText } from 'gatsby-source-contentful/rich-text'
 import * as styles from './hero.module.css'
 
 class Hero extends React.Component {
-  
 	render() {
-	  return (
-      <div className={styles.hero}>
-        {this.props.image && (
-          <GatsbyImage className={styles.image} alt={this.props.title} image={this.props.image} />
-        )}
-        <div className={styles.details}>
-          {this.props.title && ( <h1 className={styles.title}>{this.props.title}</h1> ) }
-          {this.props.content && (
-            <div className={styles.content}>{renderRichText(this.props.content)}</div>
-          )}
-        </div>
-      </div>
+		return (
+		      <div className={styles.hero}>
+			{this.props.image && (
+			  <GatsbyImage className={styles.image} alt={this.props.title} image={this.props.image} />
+			)}
+			<div className={styles.details}>
+			  {this.props.title && ( <h1 className={styles.title}>{this.props.title}</h1> ) }
+			  {this.props.content && (
+			    <div className={styles.content}>{renderRichText(this.props.content)}</div>
+			  )}
+			</div>
+		      </div>
+		)
 	}
 }
 
