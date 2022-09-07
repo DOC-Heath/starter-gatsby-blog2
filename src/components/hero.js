61c13a69f7d8d3a4f74b7e6b85526687b9b4420d
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-07 12:42:29 -0500

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { renderRichText } from 'gatsby-source-contentful/rich-text'
 
 import * as styles from './hero.module.css'
 
-const Hero = ({ image, title, content }) => (
-  <div className={styles.hero}>
-    {image && (
-      <GatsbyImage className={styles.image} alt={title} image={image} />
-    )}
-    <div className={styles.details}>
-      {title && ( <h1 className={styles.title}>{title}</h1> ) }
-      {content && (
-        <div className={styles.content}>{renderRichText(content)}</div>
-      )}
-    </div>
-  </div>
-)
+class Hero extends React.Component {
+  
+	render() {
+	  return (
+      <div className={styles.hero}>
+        {this.props.image && (
+          <GatsbyImage className={styles.image} alt={this.props.title} image={this.props.image} />
+        )}
+        <div className={styles.details}>
+          {this.props.title && ( <h1 className={styles.title}>{this.props.title}</h1> ) }
+          {this.props.content && (
+            <div className={styles.content}>{renderRichText(this.props.content)}</div>
+          )}
+        </div>
+      </div>
+	}
+}
 
 export default Hero
