15bea622708aad8058e6e57949b5912511660bd1
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-08 15:47:58 -0500

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -18,6 +18,7 @@ class Hero extends React.Component {
 			space: "5x0q4l6e6sfl"
 		})
 		this.iTick = 0
+		this.ticker = []
 	}
 	componentDidMount() {
 		this.client.getEntry('5Lst9GoxbCg66KGi2uVvW9').then(function (entry) {
@@ -34,13 +35,13 @@ class Hero extends React.Component {
 	  clearInterval(this.timerID)
 	} 
 	tick() {
-/*		const t = this.ticker[iTick]
+		const t = this.ticker[iTick]
 		this.setState({
 			image: t.fields.heroImage,
 			title: t.fields.title,
 			content: t.fields.description
 		})
-		( ++this.iTick < this.ticker.length ) || this.iTick = 0 */
+		( ++this.iTick < this.ticker.length ) || this.iTick = 0 
 	}
 	render() {
 		return (
