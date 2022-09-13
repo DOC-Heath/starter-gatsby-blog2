a4b494c7592133da9baec85d7bb99d19de0a500b
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-13 14:05:47 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ class RootIndex extends React.Component {
 		super(props)
 		this.i = 0
 		console.log(this.i)
-		this.heros = get(this, 'props.data.heros.nodes.ticks.items')
+		this.heros = get(this, 'props.data.heros.nodes.ticks')
 		console.log(this.heros)
 		const [hero0] = this.heros
 		this.state = {
@@ -103,7 +103,7 @@ export const pageQuery = graphql`
     }
     heros: allContentfulTicker( filter: { slug: { eq: "welcome" } } ) {
       nodes {
-	ticks { items { 
+	ticks {  
 		title
 		description { 
 		  raw 
@@ -118,7 +118,7 @@ export const pageQuery = graphql`
 		    width: 1180
 		  )
 		}
-	} }
+	} 
       }
     }
     
