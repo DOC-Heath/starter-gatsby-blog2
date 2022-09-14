26b6473f85c7b7f2079b1ff028a1a29e04f1c881
DOC-Heath <109249945+DOC-Heath@users.noreply.github.com>
2022-09-14 10:51:24 -0500

    Update index.js (#33)
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,25 +9,56 @@ import ArticlePreview from '../components/article-preview'
 import * as styles from '../templates/blog-post.module.css'
 
 class RootIndex extends React.Component {
+	constructor(props) {
+		super(props)
+		this.t0 = props.data.heros.firstTickTime*1000 || 9000
+		this.t  = props.data.heros.tickTime*1000 || 7000
+		if (props.data.heros.reEntry === undefined) { 
+			this.i0 = 1 
+		} else { 
+			this.i0 = props.data.heros.reEntry  
+		}
+		this.i = 0
+		this.heros = get(this, 'props.data.heros.ticks')
+		this.state = {
+		  hero: this.heros[this.i]
+		}
+	}
+	componentDidMount() {
+		this.timerID = setTimeout( () => this.tick(), this.t0 )
+	}
+	componentWillUnmount() {
+		clearTimeout(this.timerID)
+	}
+	
+	tick() {
+		this.setState({
+			hero: this.heros[this.i]
+		})
+		if ( ++this.i >= this.heros.length ) { this.i = this.i0 }
+		this.timerID = setTimeout( () => this.tick(), this.t )
+	}
+	
   render() {
     const posts = get(this, 'props.data.posts.nodes')
     const pins = get(this, 'props.data.pins.nodes')
     const pinsPosts = [...pins, ...posts]
-    const [billboard] = get(this, 'props.data.welcome.nodes')
-    
+    const h = this.state.hero
+    const welcomeBody = this.heros[0].body
+
     return (
       <Layout location={this.props.location}>
-        <Hero
-          image={billboard.heroImage.gatsbyImageData}
-          title={billboard.title}
-          content={billboard.description}
-        />
-        <div className={styles.article}>
-          <div className={styles.body}>
-            {billboard.body?.raw && renderRichText(billboard.body)}
-          </div>
-        </div>
-        <ArticlePreview posts={pinsPosts} />
+	<Hero
+	  image={h.heroImage.gatsbyImageData}
+	  title={h.title}
+	  content={h.description}
+	/>
+	<div className={styles.article}>
+	  <div className={styles.body}>
+	    {welcomeBody?.raw && renderRichText(welcomeBody)}
+	  </div>
+	</div>
+	<ArticlePreview posts={pinsPosts} />
       </Layout>
     )
   }
@@ -89,23 +120,27 @@ export const pageQuery = graphql`
         }
       }
     }
-    welcome: allContentfulPost( filter: { slug: { eq: "welcome" } } ) {
-      nodes {
-        title
-        description { 
-          raw 
-        }
-        body { 
-          raw 
-        }
-        heroImage {
-          gatsbyImageData(
-            layout: CONSTRAINED
-            placeholder: BLURRED
-            width: 1180
-          )
-        }
-      }
+    heros: contentfulTicker( slug: { eq: "home" } ) {
+    	firstTickTime
+	tickTime
+	reEntry
+	ticks {  
+		title
+		description { 
+		  raw 
+		}
+		body { 
+		  raw 
+		}
+		heroImage {
+		  gatsbyImageData(
+		    layout: CONSTRAINED
+		    placeholder: BLURRED
+		    width: 1180
+		  )
+		}
+	} 
     }
+    
   }
 `
